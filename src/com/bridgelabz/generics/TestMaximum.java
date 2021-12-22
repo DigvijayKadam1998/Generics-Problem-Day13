3cd77fc230e9e33ed0a5aa7ee388301da29bf048
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-22 11:48:54 +0530

    Welcome Message

---
+++ src/com/bridgelabz/generics/TestMaximum.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.generics;
+
+import java.util.Scanner;
+
+public class TestMaximum {
+	public static void main(String[] args) {
+		Maximum max = new Maximum();
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Enter First Number: ");
+		int first = scan.nextInt();
+
+		System.out.print("Enter Second Number: ");
+		int second = scan.nextInt();
+
+		System.out.print("Enter Third Number: ");
+		int third = scan.nextInt();
+
+		max.maxInt(first, second, third);
+	}
+}
